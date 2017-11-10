401078e7e10626495d8bce26d678df22aae7b8bc
Anthony Scopatz <scopatz@gmail.com>
2017-11-09 21:16:55 -0500

    initial win

---
+++ recipe/bld.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+SET UNIX_PREFIX=%CYGWIN_PREFIX:~9%
+echo UNIX_PREFIX is %UNIX_PREFIX%
+
+SET _PY=%PYTHON%
+SET PYTHON=python
+
+bash configure --prefix %UNIX_PREFIX% --enable-static --enable-shared --disable-documentation ^
+               --with-libiconv gnu ^
+			   -with-python %PYTHON% ^
+			   --disable-libmount
+if errorlevel 1 exit 1
+
+make -j %CPU_COUNT%
+if errorlevel 1 exit 1
+
+make install
+if errorlevel 1 exit 1
+
+SET PYTHON=%_PY%
